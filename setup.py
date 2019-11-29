9531c68429181470b748e8435a9ff8e90645ea9d
0xf0f <0x0meta@gmail.com>
2019-11-30 10:28:34 +1300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='timeit-tests',
+    version='',
+    packages=['timeit_tests'],
+    url='',
+    license='',
+    author='0xf0f',
+    author_email='',
+    description=''
+)
---
+++ timeit_tests/__init__.py
@@ -0,0 +1,39 @@
+from timeit import timeit
+from functools import partial
+
+
+def ordinal(n, suffix='.'):
+    return "{}{}{}".format(
+        n,
+        "tsnrhtdd"[
+           (n/10 % 10 != 1) *
+           (n % 10 < 4)*n % 10
+           ::4
+        ],
+        suffix
+    )
+
+
+def run_tests(
+        test=partial(timeit, number=1000),
+        ranked=True,
+        **tests
+):
+    results = dict()
+    for name, func in tests.items():
+        results[name] = test(func)
+
+    if ranked:
+        ranked_keys = sorted(
+            results.keys(),
+            # key=results.__getitem__
+            key=lambda k: results[k]
+        )
+
+        for rank, key in enumerate(ranked_keys, start=1):
+            result = results[key]
+            print(ordinal(rank), key, '=', result)
+
+    else:
+        for name, result in results.items():
+            print(name, '=', result)
